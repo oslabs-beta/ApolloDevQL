77721713b310cd1897ab8ec541466e0c7d4c0f11
smack <smackaholic@gmail.com>
2020-07-30 19:38:17 -0700

    initial boilerplate for chrome extension

---
+++ src/app/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.tsx';
+// import '@babel/polyfill';
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/extension/contentScript.ts
@@ -0,0 +1,2 @@
+
+console.log('this is contentScript.ts')
