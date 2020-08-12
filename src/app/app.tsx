fd35c1ebd6be7e43f968ed59835b895529040ad8
smack <smackaholic@gmail.com>
2020-08-11 20:13:35 -0700

    listen for GraphQL network requests

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -10,13 +10,35 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 const App = () => {
   const [requestURI, setRequestURI] = useState('');
+  const [queries, setQueries] = useState([]);
+  const [mutations, setMutations] = useState([]);
+
   useEffect(() => {
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       console.log('request received on GraphiQL tab:', request);
       setRequestURI(request.apolloURI);
       sendResponse('Hello from React');
     });
-  }, []);
+    chrome.devtools.network.onRequestFinished.addListener((httpReq: any) => {
+      if (
+        httpReq.request.url === requestURI &&
+        httpReq.request.method === 'POST'
+      ) {
+        console.log('GraphQL Request: ', httpReq);
+        const query = JSON.parse(httpReq.request.postData.text);
+        if (query.query.startsWith('query')) {
+          setQueries(prevQueries => [...prevQueries, query]);
+        } else if (query.query.startsWith('mutation')) {
+          setMutations(prevMutations => [...prevMutations, query]);
+        }
+      }
+    });
+  }, [requestURI]);
+
+  useEffect(() => {
+    console.log('Current Queries', queries);
+    console.log('Current Mutations: ', mutations);
+  }, [queries, mutations]);
 
   return (
     <div>
