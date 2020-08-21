1af16452537aefc2ee46f33689209452283f5d7e
olanrewaju Makinde <lanre.developer@gmail.com>
2020-08-21 01:46:03 +0100

    mounted the extractOperationName method, added Grids back with the list of event sdiaplying within

--- src/app/Performance.tsx
+++ src/app/Performance.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
-// import Grid from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 // import Divider from '@material-ui/core/Divider';
 // import useClientEventlogs from './utils/useClientEventlogs';
+
+import {extractOperationName} from './utils/helper';
 
 interface IPerformanceData {
   events: any;
@@ -31,11 +33,9 @@ const useStyles: any = makeStyles((theme: Theme) =>
 );
 
 function Performance({events}: IPerformanceData) {
-  // const data = useClientEventlogs();
   const componentClass = useStyles();
 
   const [selectedIndex, setSelectedIndex] = React.useState(0);
-
   const handleListItemClick = (event: any, index: number, key: string) => {
     console.log('Selected Query Event :: ', event, ' with key => ', key);
     setSelectedIndex(index);
@@ -43,38 +43,46 @@ function Performance({events}: IPerformanceData) {
   // console.log('Performance Data :: ', events);
   return (
     <div className={componentClass.root}>
-      <List component="nav" aria-label="main mailbox folders" dense>
-        {Object.entries(events)
-          .filter(([, obj]: any) => obj && (obj.response || obj.request))
-          .map(([key, obj]: any, k: number) => {
-            const newobj = {
-              operation:
-                obj &&
-                obj.request &&
-                obj.request.operation &&
-                obj.request.operation.operationName
-                  ? obj.request.operation.operationName
-                  : 'Query', // stripNameFromRequest(obj.request.operation),
-              time: obj.time,
-            };
-            return (
-              <ListItem
-                key={`operation${key}`}
-                className={`${componentClass.root}`}
-                selected={selectedIndex === k}
-                onClick={event => handleListItemClick(event, k, key)}>
-                <ListItemText primary={`${newobj.operation} ${newobj.time}`} />
-              </ListItem>
-            );
-          })}
-      </List>
-      <List component="nav" aria-label="main mailbox folders" dense>
-        Operation Details List
-      </List>
-      {/* <Grid item xs={8} className={componentClass.grid}>
-          Show content details
+      <Grid container spacing={0}>
+        <Grid item xs={4} className={componentClass.grid}>
+          <List component="nav" aria-label="main mailbox folders" dense>
+            {Object.entries(events)
+              .filter(([, obj]: any) => obj && (obj.response || obj.request))
+              .map(([key, obj]: any, k: number) => {
+                // console.log(
+                //   'Pluck Fisrt of above :: ',
+                //   extractOperationName(obj),
+                // );
+                const newobj = {
+                  operation:
+                    obj &&
+                    obj.request &&
+                    obj.request.operation &&
+                    obj.request.operation.operationName
+                      ? obj.request.operation.operationName
+                      : extractOperationName(obj),
+                  time: obj.time,
+                };
+                return (
+                  <ListItem
+                    key={`operation${key}`}
+                    className={`${componentClass.root}`}
+                    selected={selectedIndex === k}
+                    onClick={event => handleListItemClick(event, k, key)}>
+                    <ListItemText
+                      primary={`${newobj.operation} ${newobj.time}`}
+                    />
+                  </ListItem>
+                );
+              })}
+          </List>
         </Grid>
-      </Grid> */}
+        <Grid item xs={8} className={componentClass.grid}>
+          <List component="nav" aria-label="main mailbox folders" dense>
+            Operation Details List
+          </List>
+        </Grid>
+      </Grid>
     </div>
   );
 }
