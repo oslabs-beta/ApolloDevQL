1ecbc56b66b0ca22ac795ca72c529cbe8dc506e8
smack <smackaholic@gmail.com>
2020-09-19 14:13:05 -0700

    cleaned up dead code

--- src/extension/contentScript.ts
+++ src/extension/contentScript.ts
@@ -1,99 +1,10 @@
 console.log('Executing contentScript.ts...');
 
-interface IApolloClientHook {
-  Apollo11Client: any;
-}
-
-// This code will be injected into the DOM of the website
-// It will set up an interval timer to try and detect the Apollo Client object
-// every 1000ms.  Once it finds it, the interval timer will be cleared
-//
-// The eventId will be null if this is very first time we are injecting this script
-// Otherwise, it will contain the pre-generated unix Epoch time of a GraphQL network event
-function detectApolloClient(
-  window: any,
-  eventId: string = null,
-  event: any = null,
-) {
-  const apolloClientHook: IApolloClientHook = {
-    Apollo11Client: null,
-  };
-
-  // console.log('Detect Apollo Client with :: ', eventId);
-
-  // Need to add this eslint global to mitigate the following error:
-  //   22:26  error    'NodeJS' is not defined       no-undef
-  /* global NodeJS */
-  let detectionInterval: NodeJS.Timeout;
-
-  // Helper function to actually detect the Apollo Client
-  // This function will be executed by the interval timer
-  function findApolloClient() {
-    if (window.__APOLLO_CLIENT__) {
-      apolloClientHook.Apollo11Client = window.__APOLLO_CLIENT__;
-      clearInterval(detectionInterval);
-
-      console.log(
-        'contentScript findApolloClient found :>>',
-        apolloClientHook.Apollo11Client,
-      );
-
-      let apolloURI = '';
-      // Check if the uri exists on the client object
-      if (
-        apolloClientHook.Apollo11Client.link &&
-        apolloClientHook.Apollo11Client.link.options &&
-        apolloClientHook.Apollo11Client.link.options.uri
-      ) {
-        apolloURI = apolloClientHook.Apollo11Client.link.options.uri;
-        // console.log('contentScript findClient - URI exists :>>', apolloURI);
-      }
-
-      // console.log('findApolloClient event', event);
-      // console.log('findApolloClient json event', JSON.parse(event));
-      const apolloURICacheEvent = {
-        eventId,
-        event,
-        type: 'URI_CACHE',
-        text: 'Apollo Client URI',
-        apolloURI,
-        apolloCache: apolloClientHook.Apollo11Client.cache.data.data,
-        queryIdCounter:
-          apolloClientHook.Apollo11Client.queryManager.queryIdCounter,
-        mutationIdCounter:
-          apolloClientHook.Apollo11Client.queryManager.mutationIdCounter,
-        requestIdCounter:
-          apolloClientHook.Apollo11Client.queryManager.requestIdCounter,
-      };
-
-      // console.log(
-      //   'contentScript findClient - posting message :>>',
-      //   apolloURICacheEvent,
-      // );
-
-      // Send a message from the injected script to the contentScript
-      // with the Apollo Client URI and the Apollo Client cache
-      window.postMessage(apolloURICacheEvent, '*');
-    }
-  }
-
-  // TODO: We are only clearing the timer if the Apollo client is found, otherwise it will
-  // keep running indefinitely.  We should consider stopping it after some extended period
-  // of time, such as 10 minutes?  The original Apollo client code stopped after 10 seconds.
-  detectionInterval = setInterval(findApolloClient, 1000);
-}
-
 // Need to inject our script as an IIFE into the DOM
 // This will allow us to obtain the __APOLLO_CLIENT__ object
 // on the application's window object.
 // https://stackoverflow.com/questions/12395722/can-the-window-object-be-modified-from-a-chrome-extension
 const injectScript = () => {
-  // if (document instanceof HTMLDocument) {
-  //   const script = document.createElement('script');
-  //   script.textContent = `;(${detectApolloClient.toString()})(window)`;
-  //   document.documentElement.appendChild(script);
-  //   script.parentNode.removeChild(script);
-  // }
   if (document instanceof HTMLDocument) {
     const s = document.createElement('script');
     s.setAttribute('data-version', chrome.runtime.getManifest().version);
@@ -192,16 +103,9 @@ window.addEventListener(
   false,
 );
 
-if (document instanceof HTMLDocument) {
-  const s = document.createElement('script');
-  s.setAttribute('data-version', chrome.runtime.getManifest().version);
-  s.src = chrome.extension.getURL('bundles/apollo.bundle.js');
-  document.body.appendChild(s);
-}
-
 // Immediately inject the detection code once the contentScript is loaded every time
 // we navigate to a new website
 // This mitigates issues where the App panel has already mounted and sent its initial
 // requests for the URI and cache, but there isn't any website loaded yet (i.e. empty tab)
 
-// injectScript();
+injectScript();
