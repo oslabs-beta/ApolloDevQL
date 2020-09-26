2d5fe569f82a4e88f37db44c538c07fcffd173ac
olanrewaju Makinde <lanre.developer@gmail.com>
2020-09-27 00:14:37 +0100

    fixed EventLogData append and reverseDebugPrint methods with vanilla tests

--- src/app/utils/managedlog/__tests__/dll.ts
+++ src/app/utils/managedlog/__tests__/dll.ts
@@ -219,6 +219,113 @@ const evtNode3: EventLogObject = {
 };
 const eventNode3 = new EventNode(evtNode3);
 
+const evtNode4: EventLogObject = {
+  event: {
+    mutation: {
+      kind: 'Document',
+      definitions: [],
+    },
+    variables: {},
+    loading: false,
+    error: null,
+  },
+  type: 'query',
+  eventId: '099087793',
+};
+const eventNode4 = new EventNode(evtNode4);
+
+const evtNode5: EventLogObject = {
+  event: {
+    mutation: {
+      kind: 'Document',
+      definitions: [
+        {
+          kind: 'OperationDefinition',
+          operation: 'mutation',
+          name: {kind: 'Name', value: 'newBookTrips'},
+          variableDefinitions: [],
+          directives: [],
+          selectionSet: {
+            kind: 'SelectionSet',
+            selections: [
+              {
+                kind: 'Field',
+                name: {kind: 'Name', value: 'newbookTrips'},
+                arguments: [
+                  {
+                    kind: 'Argument',
+                    name: {kind: 'Name', value: 'newlaunchIds'},
+                    value: {
+                      kind: 'Variable',
+                      name: {kind: 'Name', value: 'newlaunchIds'},
+                    },
+                  },
+                ],
+                directives: [],
+                selectionSet: {
+                  kind: 'SelectionSet',
+                  selections: [
+                    {
+                      kind: 'Field',
+                      name: {kind: 'Name', value: 'success'},
+                      arguments: [],
+                      directives: [],
+                    },
+                    {
+                      kind: 'Field',
+                      name: {kind: 'Name', value: 'message'},
+                      arguments: [],
+                      directives: [],
+                    },
+                    {
+                      kind: 'Field',
+                      name: {kind: 'Name', value: 'launches'},
+                      arguments: [],
+                      directives: [],
+                      selectionSet: {
+                        kind: 'SelectionSet',
+                        selections: [
+                          {
+                            kind: 'Field',
+                            name: {kind: 'Name', value: 'id'},
+                            arguments: [],
+                            directives: [],
+                          },
+                          {
+                            kind: 'Field',
+                            name: {kind: 'Name', value: 'isBooked'},
+                            arguments: [],
+                            directives: [],
+                          },
+                          {
+                            kind: 'Field',
+                            name: {kind: 'Name', value: '__typename'},
+                          },
+                        ],
+                      },
+                    },
+                    {
+                      kind: 'Field',
+                      name: {kind: 'Name', value: '__typename'},
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      ],
+      // loc: {start: 0, end: 173},
+    },
+    variables: {launchIds: ['100']},
+    loading: false,
+    error: null,
+  },
+  type: 'mutation',
+  eventId: '099087796',
+};
+const eventNode5 = new EventNode(evtNode5);
+
 dllStructure.addEventLog(eventNode1);
 // console.log(dllStructure);
 dllStructure.addEventLog(eventNode2);
@@ -227,17 +334,26 @@ dllStructure.addEventLog(eventNode3);
 // console.log(dllStructure);
 // console.log(dllStructure.map(e => e));
 
-dllStructure2.addEventLog(eventNode3);
-// dllStructure2.addEventLog(eventNode2);
+dllStructure2.addEventLog(eventNode4);
+dllStructure2.addEventLog(eventNode5);
+
+// eventNode1: '099087779',
+// eventNode2: '099087780',
+// eventNode3: '099087787',
+// eventNode4: '099087793',
+// eventNode5: '099087796',
 
 console.log('First DLL ::', dllStructure);
 console.log(dllStructure.debugPrint());
+console.log(dllStructure.reverseDebugPrint());
 console.log('Second DLL ::', dllStructure2);
 console.log(dllStructure2.debugPrint());
+console.log(dllStructure2.reverseDebugPrint());
 
 dllStructure.append(dllStructure2);
 console.log('New DLL ::', dllStructure);
 console.log(dllStructure.debugPrint());
+console.log(dllStructure.reverseDebugPrint());
 
 // const allStructures = dllStructure.addEventLog(dllStructure2.eventHead);
 // const allStructures = dllStructure.insertEventLogAfter(
