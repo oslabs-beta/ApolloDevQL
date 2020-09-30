3da95363c91f13cb446b0ba5353e651ca18acbf3
olanrewaju Makinde <lanre.developer@gmail.com>
2020-09-30 21:38:23 +0100

    WIP : Mounted Contex Provider to app and its index

--- src/app/Panel/index.tsx
+++ src/app/Panel/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './app';
+import CustomThemeProvider from './themes/CustomThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <CustomThemeProvider>
+    <CssBaseline />
+    <App />
+  </CustomThemeProvider>,
+  document.getElementById('root'),
+);
