f76b33313aa869ad7dd1373155231cea2de8b39a
olanrewaju Makinde <lanre.developer@gmail.com>
2020-09-30 21:36:54 +0100

    WIP Context Provider with site wide theme setup

---
+++ src/app/Panel/themes/dark.ts
@@ -0,0 +1,29 @@
+import {createMuiTheme} from '@material-ui/core/styles';
+import {red} from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#26292C',
+      light: 'rgb(81, 91, 95)',
+      dark: 'rgb(26, 35, 39)',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#FFB74D',
+      light: 'rgb(255, 197, 112)',
+      dark: 'rgb(200, 147, 89)',
+      contrastText: 'rgba(0, 0, 0, 0.87)',
+    },
+    // titleBar: {
+    //   main: '#555555',
+    //   contrastText: '#ffffff',
+    // },
+    error: {
+      main: red.A400,
+    },
+  },
+});
+
+export default theme;
---
+++ src/app/Panel/themes/normal.ts
@@ -0,0 +1,25 @@
+import {createMuiTheme} from '@material-ui/core/styles';
+import {red} from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#cc4444',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#f5f5f5',
+    },
+    // titleBar: {
+    //   main: '#eeeeee',
+    //   contrastText: '#222222',
+    // },
+  },
+});
+
+export default theme;
