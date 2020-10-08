e1015a8c9eb1bb73a9094a778d4a131644922977
Matt Digel <med5342@gmail.com>
2020-10-07 19:56:20 -0700

    added 3 tests for the Event Log

--- src/app/Performance_Tab/__tests__/Performance_test.js
+++ src/app/Performance_Tab/__tests__/Performance_test.js
@@ -1,34 +1,75 @@
 // https://www.smashingmagazine.com/2020/06/practical-guide-testing-react-applications-jest/
 import React from 'react';
-import {
-  configure
-} from 'enzyme';
+import {configure, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import Performance from '../Performance';
+// Project Files
+import Performance from '../Performance_v2';
 import mountWithTheme from '../../../../__mocks__/themeMock';
-
+import Apollo11ThemeContext from '../../Panel/themes/ThemeProvider';
 import fakeNetworkEvents from '../../../../__mocks__/fakeNetworkEvent';
-
-console.log('networkEvents in tests', fakeNetworkEvents);
 
 configure({
   adapter: new Adapter(),
 });
 
 describe('snapshot tests', () => {
-      const networkEvents = {};
-      let wrapper;
+  const networkEvents = {};
+  let wrapper;
 
-      it('should mount the app', () => {
-          wrapper = mountWithTheme( < Performance networkEvents = {
-              networkEvents
-            }
-            />);
-            expect(wrapper).toMatchSnapshot();
-          });
-      });
+  it('should mount the app', () => {
+    wrapper = mountWithTheme(<Performance networkEvents={networkEvents} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
 
-    describe('Events Log', () => {
-      expect(1).toEqual(1);
+describe('Events Log', () => {
+  it('the first event should be named: GetLaunchList', () => {
+    // Helpful Blog: https://medium.com/7shifts-engineering-blog/testing-usecontext-react-hook-with-enzyme-shallow-da062140fc83
+
+    const wrapper = mountWithTheme(
+      <Performance networkEvents={fakeNetworkEvents} />,
+    );
+
+    const eventListElement = wrapper
+      .find({
+        className:
+          'MuiTypography-root MuiListItemText-primary MuiTypography-body2 MuiTypography-displayBlock',
+      })
+      .first()
+      .find('span')
+      .text();
+
+    expect(eventListElement).toContain('GetLaunchList');
+  });
+
+  it('the first event should have speed of 269 ms', () => {
+    const wrapper = mountWithTheme(
+      <Performance networkEvents={fakeNetworkEvents} />,
+    );
+
+    const eventListElement = wrapper
+      .find({
+        className:
+          'MuiTypography-root MuiListItemText-primary MuiTypography-body2 MuiTypography-displayBlock',
+      })
+      .first()
+      .find('span')
+      .text();
+
+    expect(eventListElement).toContain('269 ms');
+  });
+
+  it('there should be 6 events + spinner', () => {
+    const wrapper = mountWithTheme(
+      <Performance networkEvents={fakeNetworkEvents} />,
+    );
+
+    const eventListElement = wrapper.find({
+      className:
+        'MuiTypography-root MuiListItemText-primary MuiTypography-body2 MuiTypography-displayBlock',
     });
+
+    expect(eventListElement).toHaveLength(7);
+  });
+});
